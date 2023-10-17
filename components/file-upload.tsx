54d6d0bc91e2dada9bfe5c32dd6acf39e6d6dcc3
bilguun.z <bilguun.z@iMac.local>
2023-10-17 09:55:46 +0800

    added uploadthing component

---
+++ components/file-upload.tsx
@@ -0,0 +1,40 @@
+import { UploadDropzone } from "@/lib/upload-thing";
+import { X } from "lucide-react";
+import "@uploadthing/react/styles.css";
+import Image from "next/image";
+
+interface Props {
+  endpoint: "messageFile" | "serverImage";
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+const FileUpload = ({ endpoint, value, onChange }: Props) => {
+  const fileType = value?.split(".")?.pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative w-20 h-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 absolute top-0 right-0 p-1 rounded-full text-white"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        console.log(res?.[0]);
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(err: Error) => {
+        console.log(err, "ERROR");
+      }}
+    />
+  );
+};
+export default FileUpload;
--- components/modal/initial-modal.tsx
+++ components/modal/initial-modal.tsx
@@ -22,20 +22,21 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
-
+import FileUpload from "../file-upload";
 const formSchema = z.object({
   name: z.string().min(1, {
     message: "Server name is required",
   }),
-  //   image: z.string().min(1, {
-  //     message: "Server image is required",
-  //   }),
+    image: z.string().min(1, {
+      message: "Server image is required",
+    }),
 });
 const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
   useEffect(() => {
     setIsMounted(true);
   }, []);
+  
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -46,6 +47,7 @@ const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log('kkk')
     console.log(values);
   };
   if (!isMounted) return null;
@@ -65,7 +67,23 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                UPLOAD
+                <FormField 
+                  control={form.control}
+                  name="image"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    <FormMessage />
+
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
