56a1abc25614ee3c810012f3a7fe835fc258847d
bilguun.z <bilguun.z@iMac.local>
2023-10-19 19:09:32 +0800

    added members modal

--- components/modal/edit-server-modal.tsx
+++ components/modal/edit-server-modal.tsx
@@ -1,118 +1,98 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-  } from "@/components/ui/dialog";
-  import useModal from "@/hooks/use-modal-store";
-  import { zodResolver } from "@hookform/resolvers/zod";
-  import axios from "axios";
-  import { useRouter } from "next/navigation";
-  import { useForm } from "react-hook-form";
-  import * as z from "zod";
-  import FileUpload from "../file-upload";
-  import { Button } from "../ui/button";
-  import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "../ui/form";
-  import { Input } from "../ui/input";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import useModal from "@/hooks/use-modal-store";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import FileUpload from "../file-upload";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 import { useEffect } from "react";
-  const formSchema = z.object({
-    name: z.string().min(1, {
-      message: "Server name is required",
-    }),
-    imageUrl: z.string().min(1, {
-      message: "Server image is required",
-    }),
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required",
+  }),
+});
+const EditServerModal = () => {
+  const router = useRouter();
+  const { type, onClose, isOpen, data } = useModal();
+  const isModalOpen = type === "editServer" && isOpen;
+  const server = data?.server;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
   });
-  const EditServerModal = () => {
-    const router = useRouter();
-    const {type, onClose, isOpen, data} = useModal();
-    const isModalOpen = type === 'editServer' && isOpen;
-    const server = data?.server;
-    const form = useForm({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        name: "",
-        imageUrl: "",
-      },
-    });
-    const isLoading = form.formState.isSubmitting;
-  
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-      try {
-        await axios.patch(`/api/servers/${server?.id}`, values);
-        form.reset();
-        router.refresh();
-        onClose();
-      } catch (error) {
-        console.log("ERROR: ", error);
-      }
-    };
-    const handleClose = () => {
-      onClose();
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/servers/${server?.id}`, values);
       form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log("ERROR: ", error);
     }
-    useEffect(() => {
-        if (server) {
-            form.setValue('name',  server.name);
-            form.setValue('imageUrl', server.imageUrl)
-        }
-    }, [server, form])
-    return (
-      <Dialog open={isModalOpen} onOpenChange={handleClose}>
-        <DialogContent className="bg-white text-dark p-0 overflow-hidden">
-          <DialogHeader className="pt-8 px-6">
-            <DialogTitle className="text-2xl text-center font-bold text-zinc-800">
-              Customize your server
-            </DialogTitle>
-            <DialogDescription className="text-center text-zinc-500">
-              Give your server a personality with a name and a image. You can
-              always change it later.
-            </DialogDescription>
-          </DialogHeader>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className="space-y-8 px-6">
-                <div className="flex items-center justify-center text-center">
-                  <FormField
-                    control={form.control}
-                    name="imageUrl"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <FileUpload
-                            endpoint="serverImage"
-                            value={field.value}
-                            onChange={field.onChange}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+  };
+  const handleClose = () => {
+    onClose();
+    form.reset();
+  };
+
+  useEffect(() => {
+    if (server) {
+      form.setValue("name", server.name);
+      form.setValue("imageUrl", server.imageUrl);
+    }
+  }, [server, form]);
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-dark p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold text-zinc-800">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and a image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
                 <FormField
                   control={form.control}
-                  name="name"
+                  name="imageUrl"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                        Server name
-                      </FormLabel>
                       <FormControl>
-                        <Input
-                          disabled={isLoading}
-                          className="bg-zinc-300/50 border-0 focus-visible:ring:0 text-black focus-visible:ring-offset-0"
-                          placeholder="Enter server name"
-                          {...field}
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
                         />
                       </FormControl>
                       <FormMessage />
@@ -120,17 +100,37 @@ import { useEffect } from "react";
                   )}
                 />
               </div>
-              <DialogFooter className="bg-gray-100 px-6 py-4">
-                <Button variant="primary" disabled={isLoading}>
-                  Create
-                </Button>
-              </DialogFooter>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
-    );
-  };
-  
-  export default EditServerModal;
-  
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Server name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring:0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EditServerModal;
--- components/modal/member-modal.tsx
+++ components/modal/member-modal.tsx
@@ -3,76 +3,140 @@ import {
   DialogContent,
   DialogDescription,
   DialogHeader,
-  DialogTitle
+  DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import UserAvatar from "@/components/user-avatar";
 import useModal from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
-import { MoreVertical, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
+import { MemberRole } from "@prisma/client";
+import {
+  Check,
+  Gavel,
+  Loader2,
+  MoreVertical,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuSub, DropdownMenuSubTrigger, DropdownMenuTrigger } from "../ui/dropdown-menu";
-  const roleIconMap = {
-    "GUEST": null,
-    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500"/>,
-    "ADMIN": <ShieldAlert className="h-4 w-4 text-rose-500"/>
-  }
-  const MemberModal = () => {
-    const router = useRouter();
-    const {type, onClose, isOpen, data} = useModal();
-    const [loadingId, setLoadingId] = useState('');
-    const isModalOpen = type === 'manageMembers' && isOpen;
-    const server: ServerWithMembersWithProfiles = data?.server;
-    
-    return (
-      <Dialog open={isModalOpen} onOpenChange={onClose}>
-        <DialogContent className="bg-white text-dark overflow-hidden">
-          <DialogHeader className="pt-8 px-6">
-            <DialogTitle className="text-2xl text-center font-bold text-zinc-800">
-              ManageMembers
-            </DialogTitle>
-            <DialogDescription className="text-center text-zinc-500">
-              {server?.members?.length} Members
-            </DialogDescription>
-          </DialogHeader>
-          <ScrollArea className="mt-8 max-h-[420px] pr-6">
-              {server?.members?.map((member) => (
-                <div key={member.id} className="flex items-center gap-x-2 mb-6">
-                  <UserAvatar src={member?.profile?.imageUrl}/>
-                  <div className="flex flex-col gap-y-1">
-                    <div className="text-xs font-semibold flex items-center gap-x-1 text-black">
-                      {member?.profile?.name}
-                      {roleIconMap[member?.role]}
-                    </div>
-                    <p className="text-xs text-zinc-500">
-                      {member?.profile?.email}
-                    </p>
-                  </div>
-                  {/* {server.profileId !== member?.profileId && ( */}
-                    <div className="ml-auto">
-                      <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <MoreVertical className="w-4 h-4"/>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent side='left'>
-                          <DropdownMenuSub>
-                            <DropdownMenuSubTrigger className="flex items-center">
-                              <ShieldQuestion className="w-4 h-4 mr-2"/>
-                              <span>Role</span>
-                            </DropdownMenuSubTrigger>
-                          </DropdownMenuSub>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </div>
-                  {/* )} */}
-                </div>
-              ))}
-          </ScrollArea>
-        </DialogContent>
-      </Dialog>
-    );
+import qs from 'query-string';
+import axios from "axios";
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
+const MemberModal = () => {
+  const router = useRouter();
+  const { type, onClose, isOpen, data, onOpen } = useModal();
+  const [loadingId, setLoadingId] = useState("");
+  const isModalOpen = type === "manageMembers" && isOpen;
+
+  const server: ServerWithMembersWithProfiles = data?.server;
+
+  const onRoleChange = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+          memberId,
+        }
+      });
+      const resp = await axios.patch(url, {role});
+      router.refresh();
+      onOpen('manageMembers', {server: resp.data});
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingId('');
+    }
   };
-  
-  export default MemberModal;
-  
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-dark overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold text-zinc-800">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member?.profile?.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center gap-x-1 text-black">
+                  {member?.profile?.name}
+                  {roleIconMap[member?.role]}
+                </div>
+                <p className="text-xs text-zinc-500">
+                  {member?.profile?.email}
+                </p>
+              </div>
+              {/* {server.profileId !== member?.profileId && ( */}
+              <div className="ml-auto">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <MoreVertical className="w-4 h-4" color="black" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent side="left">
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger className="flex items-center">
+                        <ShieldQuestion className="w-4 h-4 mr-2" />
+                        <span>Role</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem onClick={() => onRoleChange(member.id, 'GUEST')}>
+                            <Shield className="h-4 w-4 mr-2" />
+                            Guest
+                            <Check className="h-4 w-4 ml-auto" />
+                          </DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => onRoleChange(member.id, "MODERATOR")}>
+                            <Shield className="h-4 w-4 mr-2" />
+                            Moderator
+                            <Check className="h-4 w-4 ml-auto" />
+                          </DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <Gavel className="h-4 w-4 mr-2" />
+                      Kick
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              {/* )} */}
+              {loadingId == member.id && (
+                <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4"/>
+              )}
+            </div>
+          ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MemberModal;
